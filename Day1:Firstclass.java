72e80593d3973db53c00816889c1ba670c3c9e07
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2025-08-20 01:50:26 +0530

    commit11and12

---
+++ Day1:Firstclass.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Firstclass{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int number = sc.nextInt();
+        System.out.println(number*1);
+        System.out.println(number*2);
+        System.out.println(number*3);
+        System.out.println(number*4);
+        System.out.println(number*5);
+        System.out.println(number*6);
+        System.out.println(number*7);
+        System.out.println(number*8);
+        System.out.println(number*9);
+        System.out.println(number*10);
+        System.out.println(number*11);
+        System.out.println(number*12);
+
+        
+    }
+}
---
+++ Day2:voting.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class voting
+{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int age=sc.nextInt();
+    if(age>=18){
+      System.out.println("you are eligible to vote");
+
+    }
+    else{
+      System.out.println("you are not allowed to vote");
+    }
+
+  }
+}
