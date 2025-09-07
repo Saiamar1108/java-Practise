32133241fdd67b54287ba0359fd0eff38f5a9a56
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2025-09-07 23:06:07 +0530

    Add advanced pattern programs and update pyramids
    Added new Java files for butterfly, diamond, rhombus, and numeric pyramid patterns in the advancedpatterns directory. Introduced palindrome number pyramid in patterns. Refactored numberpyramid.java for correct pyramid formatting.

--- advancedpatterns/rhombus.java
+++ advancedpatterns/rhombus.java
@@ -1,23 +1,23 @@
-package patterns;
+package advancedpatterns;
 import java.util.*;
 
-public class numberpyramid {
+public class rhombus {
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     for(int i=1;i<=n;i++){
-      for (int j=1;j<=n-i;j++){
+      for(int j=1;j<=n-i;j++){
         System.out.print(" ");
       }
-      for (int j=1;j<=i;j++){
-        System.out.print(i+" ");
+      for(int j=1;j<=n;j++){
+        System.out.print("* ");
 
-    }System.out.println();
 
     }
 
     
-    
-    }}
+    System.out.println();
+    }
+  }
 
-
+}
