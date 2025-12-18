018b9e1b24da490cd2a9a9657455241d4315a9e0
A.Sai Amar Chaitanya <saiamar110869@gmail.com>
2025-12-18 18:35:19 +0530

    new start
    from scaler

---
+++ variables.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class variables {
+  
+    public static void main(String[] args) {
+
+        String name="sai amar";
+        char a=name.charAt(2);
+        System.out.println(a);
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println(x*x);;
+
+
+
+
+    }
+}
